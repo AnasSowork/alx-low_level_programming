68c3fe687980f7ec7b144cf0b2d94be960423102
anasf <anas.001falaki@gmail.com>
2023-06-21 17:47:36 +0100

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -22,7 +22,8 @@ void times_table(void)
 				_putchar(',');
 				_putchar(' ');
 				_putchar(' ');
-				_putchar(k + '0');} 
+				_putchar(k + '0');
+			}
 			else if (k >= 10)
 			{
 				_putchar(',');
