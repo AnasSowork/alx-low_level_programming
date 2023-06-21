fd4457e96201d37573c02895e952af042b5480ac
anasf <anas.001falaki@gmail.com>
2023-06-21 19:47:29 +0100

    103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -5,12 +5,11 @@
  * Return: Always 0 (Success)
  */
 int main(void)
-int i;
-unsigned long int j, k, next, sum;
-
-j = 1;
-k = 2;
-sum = 0;
+{
+	long unsigned int i, j, k, next, sum;
+	j = 1;
+	k = 2;
+	sum = 0;
 
 for (i = 1; i <= 33; ++i)
 {
