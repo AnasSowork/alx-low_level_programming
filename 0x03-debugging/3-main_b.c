b9a32c093e5c97283c43d8c32e730da71ce1e514
anasf <anas.001falaki@gmail.com>
2023-07-04 02:49:48 +0100

    all of it

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+#include "3-print_remaining_days.c"
+#include "3-convert_day.c"
+
+/**
+  * main - takes a date and prints how many days are left in the year, taking
+  * leap years into account
+  * Return: 0
+  */
+
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+
+	month =29;
+	day = 02;
+	year = 2000;
+
+	printf("Date: %02d/%02d/%04d\n", day, month, year);
+
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+
+	return (0);
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+#C - Even more pointers, arrays and strings
+
+#TASKS.
+
+0. memset
+
+Write a function that fills memory with a constant byte.
