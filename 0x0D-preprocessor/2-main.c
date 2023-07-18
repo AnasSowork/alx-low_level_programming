f2ec47f94a27987a9492f1aa2ed16689f0078a80
anasf <anas.001falaki@gmail.com>
2023-07-18 03:56:16 +0100

    __FILE__

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * 
- * 
- * 
-*/
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-    printf("%s\n", __FILE__);
-    return (0);
+printf("%s\n", __FILE__);
+return (0);
 }
