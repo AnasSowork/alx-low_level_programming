26c5d9ebe33579bb97316ffc8ea7184dea87a394
anasf <anas.001falaki@gmail.com>
2023-07-18 21:41:49 +0100

    dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - is a tructure that contanes all the elements
+ *
+ * @name: the name of the dog .
+ *
+ * @age: the age that the dog have .
+ *
+ * @owner: the owner of the dog .
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
