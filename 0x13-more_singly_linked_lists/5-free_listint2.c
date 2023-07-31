ec3d345e7e2856f60868a610a7e64acc588c922e
anasf <anas.001falaki@gmail.com>
2023-07-31 23:55:36 +0100

    safi rah salaw

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+/**
+ * free_listint2 - is a function that free the memory
+ * @head: is the head of a linked list
+ *
+*/
+void free_listint2(listint_t **head)
+{
+	listint_t *current = *head;
+	listint_t *next;
+
+	while (current != NULL)
+	{
+	next = current->next;
+	free(current);
+	current = next;
+	}
+	*head = NULL;
+}
