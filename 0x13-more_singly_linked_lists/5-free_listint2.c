33e3ed5a2b34737d40885f5fb5eed5bb25118293
anasf <anas.001falaki@gmail.com>
2023-08-01 01:05:59 +0100

    safi rah salaw

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -11,6 +11,10 @@ void free_listint2(listint_t **head)
 	listint_t *current = *head;
 	listint_t *next;
 
+	if (head == NULL || *head == NULL)
+	{
+		return;
+	}
 	while (current != NULL)
 	{
 	next = current->next;
