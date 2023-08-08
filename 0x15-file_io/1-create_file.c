2905bd5aecc21d041ad878895a38f6a19c8f6cde
Anas <anas.001falaki@gmail.com>
2023-08-08 14:48:38 +0100

    wa hanta

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,31 +11,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int filedesc;
+	int filedesc, w, len = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	filedesc = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	filedesc = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	w = write(filedesc, text_content, len);
 
-	if (filedesc == -1)
-	{
+	if (filedesc == -1 || w == -1)
 		return (-1);
-	}
-
-	if (!text_content)
-	{
-		ssize_t bytes_written = write(filedesc, text_content, strlen(text_content));
-
-		if (bytes_written < 0)
-		{
-			close(filedesc);
-			return (-1);
-		}
-	}
 
 	close(filedesc);
+
 	return (1);
 }
